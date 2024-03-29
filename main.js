c005aed2125edbb5433497e7bd40a4d910537f27
Vladislav Keller <skiffvlad90@gmail.com>
2024-03-29 19:07:11 +0200

    ччсчс

--- main.js
+++ main.js
@@ -10,10 +10,11 @@
 // const promiseA = makePromise("promiseA value", 1000);
 // const promiseB = makePromise("promiseB value", 3000);
 // const promiseC = makePromise("promiseC value", 500);
-// Promise.all([promiseA, promiseB,promiseC])
-//   .then((value) => console.log(value))
+// Promise.all([promiseA, promiseB, promiseC])
+//   .then((value) => console.log(value[0], value))
 //   .catch((error) => console.log(error));
 
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
 // Метод Promise.race() - буде чекати того хто найшвидже виконає обіцянку з будь-яким результатом
 // const makePromise = (text, delay) => {
 //   return new Promise((resolve) => {
@@ -27,22 +28,97 @@
 //   .then((value) => console.log(value))
 //   .catch((error) => console.log(error));
 
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
 // метод Promise.any()
-// let promise = Promise.any(iterable);
-Promise.any([
-  new Promise((resolve, reject) =>
-    setTimeout(() => reject(new Error("Упс!"), 1000))),
-  new Promise((resolve,reject) => setTimeout(() => reject(new Error("Помилка!"), 3000))),
-  new Promise((resolve,reject) => setTimeout(() => reject(new Error("Все пропало!"), 2000))),
-//   new Promise((resolve,reject) => setTimeout(() => resolve(1),500)),
-//   new Promise((resolve,reject) => setTimeout(() => resolve(43),200))
-])
-.then(resolve => console.log(resolve))
-.catch(error => {
-    console.log(error.constructor.name);
-    console.log(error.errors);
-    // можна виводить або масив момилок або по окремості
-    // console.log(error.errors[1]);
-    // console.log(error.errors[2]);
-    console.log(error.errors[0])
-});
+// Promise.any([
+//   new Promise((resolve, reject) =>
+//     setTimeout(() => reject(new Error("Упс!"), 1000))
+//   ),
+//   new Promise((resolve, reject) =>
+//     setTimeout(() => reject(new Error("Помилка!"), 3000))
+//   ),
+//   new Promise((resolve, reject) =>
+//     setTimeout(() => reject(new Error("Все пропало!"), 2000))
+//   ),
+//   // new Promise((resolve, reject) => setTimeout(() => resolve(1), 500)),
+//   // new Promise((resolve, reject) => setTimeout(() => resolve(43), 200)),
+// ])
+//   .then((resolve) => console.log(resolve))
+//   .catch((error) => {
+//     console.log(error.constructor.name);
+//     console.log(error.errors);
+//     // можна виводить або масив момилок або по окремості
+//     // console.log(error.errors[1]);
+//     // console.log(error.errors[2]);
+//     console.log(error.errors[0]);
+//   });
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!//
+// Методи Promise.resolve() та Promise.reject()
+// (Статичні методи для створення промісів,миттєво успішно виконуються або відхиляються)
+
+// Fulfiled promise
+// new Promise((resolve) => resolve("Success value")).then((value) =>
+//   console.log(value)
+// );
+// Promise.resolve("Promise.resolveSuccess value").then((value) =>
+//   console.log(value)
+// );
+
+//Rejected promise
+// new Promise((resolve, reject) => reject("Error")).catch((error) =>
+//   console.log(error)
+// );
+// Promise.reject("Promise.reject error catch").catch((error) =>
+//   console.log(error)
+// );
+// Тепер трошки практики на більш конкретних прикладах
+// const makeGreeting = (guestName) => {
+//   if (guestName === "" || guestName === undefined) {
+//     return {
+//       success: false,
+//       message: "Guest naame must not be empty",
+//     };
+//   }return {
+//     succes: true,
+//     massege: `Welcome ${guestName}`,
+//   };
+// };
+
+// const result = makeGreeting("Polly");
+// if (result.succes) {
+//   console.log(result.message);
+// } else {
+//   console.error(result.message);
+// }
+
+// const makeGreeting = (guestName, onSuccess, onError) => {
+//   if (guestName === "" || guestName === undefined) {
+//     return onError("Guest name must not be empty");
+//   }
+//   onSuccess(`Welcome ${guestName}!Have a nice day!`);
+// };
+
+// makeGreeting(
+//   "Polly",
+//   (greeting) => console.log(greeting),
+//   (error) => console.error(error)
+// );
+
+// Останнім кроком іде промісифікація самої функції
+// const makeGreeting = (guestName) => {
+//   if (guestName === "" || guestName === undefined) {
+//     return Promise.reject("Guest name must not be empty");
+//   }
+//   return Promise.resolve(`Welcome ${guestName}...`);
+// };
+// makeGreeting("Jeremy")
+//   .then((greeting) => console.log(greeting))
+//   .catch((error) => console.error(error));
+
+// makeGreeting("Jully")
+//   .then((greeting) => console.log(greeting))
+//   .catch((error) => console.error(error));
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+// Цикл подій
